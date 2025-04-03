635f1257c91908c0df9eec9007f5c05868454bba
LorenzoMiles <148791566+LorenzoMiles@users.noreply.github.com>
2025-04-03 12:13:04 -0500

    Update urls.py

--- SEFinalProject/SEApp/urls.py
+++ SEFinalProject/SEApp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 # Define a List of url patterns
 urlpatterns = [
-    path('', views.index)
+    path('<int:id>', views.index, name = "index"),
+    path("", views.home, name ="home")
 ]
