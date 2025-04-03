8529f902b4fcb7e068fccc9e8e5727cfe241733d
LorenzoMiles <148791566+LorenzoMiles@users.noreply.github.com>
2025-04-03 12:12:36 -0500

    Update views.py

--- SEFinalProject/SEApp/views.py
+++ SEFinalProject/SEApp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def index(request):
-    return HttpResponse("3340 Web App")
+def index(response, id):
+    return render(response, "SEApp/base.html", {})
+
+def home(response):
+    return render(response, "SEApp/home.html", {})
